64c39d7e42f90ec8165f99c93f4c1a9cd94df77e
caio caminha <caminhacaiopro@gmail.com>
2021-05-18 16:04:54 -0300

    implementadas todas as rotas

--- server/controllers/DocController.js
+++ server/controllers/DocController.js
@@ -3,7 +3,7 @@ const DocService = require("../services/DocService");
 const router = Router();
 
 router.get('/docs', DocService.findAllDocs);
-router.get('/docs:id', DocService.findOneDoc)
+router.get('/docs:id', DocService.findOneDoc);
 router.post('/docs', DocService.createDoc);
 router.put('docs/:id', DocService.updateDoc);
 router.delete('/docs:id', DocService.deleteDoc);
