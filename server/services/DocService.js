bd8859ed3c678ba075647e810aa5d02504b1f2d0
caio caminha <caminhacaiopro@gmail.com>
2021-05-18 15:56:25 -0300

    metedo de delete implementado

--- server/services/DocService.js
+++ server/services/DocService.js
@@ -45,6 +45,16 @@ class DocService{
         }
     }
 
+    static async deleteDoc(req, res){
+        const { id } = req.params;
+        try{
+            database.Doc.destroy({where: {id: Number(id)}})
+            return res.status(200).json({message: "Documentação deletada com sucesso!"});
+        }catch(e){
+            return res.status(500).json(e.message);
+        }
+    }
+
 }
 
 
